69471fc5528cfe6c2d54f87e1d4b23a9d9fd7f14
yihsunphys <yqiu789@gmail.com>
2025-03-04 15:03:19 +0800

    20250304

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template    
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from config import Config  
+from forms import RegisterForm 
 
 app = Flask(__name__)   
+bootstrap = Bootstrap(app)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
